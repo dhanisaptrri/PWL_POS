0242a2810dd6fec5074e26b19415853b4c9b5573
dhanisaptrri <shadhanisa2@gmail.com>
2025-03-04 03:34:15 +0700

    Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,21 +9,23 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        $user = UserModel::firstOrNew(
-            ['username' => 'manager33'], // Cari berdasarkan username
-            [
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
-    
-        if (!$user->exists) { // Cek apakah user sudah ada di database
-            $user->save();
-        }
-    
-        return view('user', ['data' => $user]);
+    public function index()
+    {
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        $user->wasChanged(['nama', 'username']); // true
     }
-    
 }
