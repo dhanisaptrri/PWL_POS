1966395d2a7f855612bdf12d964c0da32fc43f38
dhanisaptrri <shadhanisa2@gmail.com>
2025-03-04 02:59:26 +0700

    Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-{
-    $users = UserModel::where('level_id', 2)->count();
-    //dd($users); // Debug untuk melihat data
-    return view('user', ['data' => $users]);
-}
-
+    public function index(){
+        $user = UserModel::firstOrNew(
+            ['username' => 'manager33'], // Cari berdasarkan username
+            [
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+    
+        if (!$user->exists) { // Cek apakah user sudah ada di database
+            $user->save();
+        }
+    
+        return view('user', ['data' => $user]);
+    }
+    
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,14 +4,23 @@
         <title>Data User</title>
     </head>
     <body>
-        <h1><strong>Data User</strong></h1>
+        <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
             </tr>
+
+           
             <tr>
-                <td>{{ $data }}</td> {{-- Menampilkan jumlah pengguna --}}
+                <td>{{ $data->user_id }}</td> {{-- Pastikan kolom di database adalah "id" --}}
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
+            
         </table>
     </body>
 </html>
