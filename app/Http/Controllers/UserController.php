2c1772fe8445dc16fe9dfb913f4847c658b8af2f
dhanisaptrri <shadhanisa2@gmail.com>
2025-03-03 09:54:24 +0700

    Praktikum 1 - $fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,19 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'nama' => 'Pelanggan Pertama',
-            ];
-            UserModel:: where('username', 'customer-1')->update($data); // update data user
-            // coba akses model UserModel
-            $user = UserModel:: all(); // ambil semua data dari tabel m_user
-            return view('user', ['data' => $user]);
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+
+        // Simpan data ke database
+        UserModel::create($data);
+
+        // Ambil semua data dari tabel m_user
+        $user = UserModel::all();
+
+        // Pastikan view 'user' ada di resources/views/user.blade.php
+        return view('user', ['data' => $user]);
     }
 }
--- app/Models/USerModel.php
+++ app/Models/USerModel.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserModel extends Model {
-
+class UserModel extends Model
+{
     use HasFactory;
 
-    protected $table = 'm_user';
+    protected $table = "m_user";
+    protected $primaryKey = "id";
 
-    protected $primaryKey = 'user_id';
-
+    // The attributes that are mass assignable.
+    /**
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
